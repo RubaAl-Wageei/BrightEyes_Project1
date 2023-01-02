6f98ec9720c721daabf728220815fa69e8462b8f
unknown <ruba.salahuddin@gmail.com>
2023-01-02 13:39:53 +0300

    Edit

--- publicUser/include/header.php
+++ publicUser/include/header.php
@@ -84,11 +84,8 @@ textarea {
             <div class="row">
                 <div class="col-xl-3 col-lg-2">
                     <div class="header__logo">
- main
-                        <a href="./index.php"><img style="width:50px" src="img/logo_1.png" alt=""></a> <?php //----------------logo ?>
-=======
+
                         <a href="./index.php"><img src="./img/logo_1.png" alt="" style="width:50px"></a> 
- main
                     </div>
                 </div>
                 <div class="col-xl-6 col-lg-7">
@@ -106,7 +103,7 @@ textarea {
                                 </li>     
                             <!-- <li><a href="#">Men’s</a></li> -->
                          
-                            <!-- <li><a href="./blog.html">Blog</a></li> -->
+                            <li><a href="./shop.php">SHOP</a></li>
                             <li><a href="./contact.php">Contact</a></li>
                             <li><a href="./about.php">About us</a></li>
                         </ul>

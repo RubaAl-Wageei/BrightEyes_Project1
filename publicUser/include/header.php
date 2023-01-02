88e40c788af602b7942a1cd2469404854a0fe3e2
Asem <asemyassen300@gmail.com>
2023-01-02 13:00:52 +0300

    Changes

--- publicUser/include/header.php
+++ publicUser/include/header.php
@@ -83,7 +83,7 @@ textarea {
             <div class="row">
                 <div class="col-xl-3 col-lg-2">
                     <div class="header__logo">
-                        <a href="./index.php"><img style="width:50px" src="img/logo_1.png" alt=""></a> <?php //----------------logo ?>
+                        <!-- <a href="./index.php"><img style="width:50px" src="img/logo_1.png" alt=""></a> -->
                     </div>
                 </div>
                 <div class="col-xl-6 col-lg-7">
